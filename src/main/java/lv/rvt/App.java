283ed6431e13c4f63295fd68dc835afeb34b9a4f
23DP3KKole <166013436+23DP3KKole@users.noreply.github.com>
2024-09-06 08:15:58 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,23 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner myScan = new Scanner(System.in);
 
+        System.out.println("Print your name");
+        String name = myScan.nextLine();
+
+        System.out.println("Print your surname");
+        String surname = myScan.nextLine();
+
+        System.out.println("Print group which you are attended to");
+        String group = myScan.nextLine();
+
+        System.out.println("Students: " + name + " " + surname + ", grupa " + group);
     }
 
 }
