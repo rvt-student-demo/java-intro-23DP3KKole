4822eb3ea96f0a942e86e27595c1618efe3ae16c
23DP3KKole <166013436+23DP3KKole@users.noreply.github.com>
2024-10-23 07:41:22 +0000

    Only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -12,14 +12,27 @@ public class App
 
         while (true){
             int input = Integer.valueOf(scanner.nextLine());
-            if(input != 0){
+            if(input != -1){
                 nameList.add(input);
-            }else if(input == 0){
-                nameList.add(input);
+            }else if(input == -1){
                 break;
             }
         }
 
-        System.out.println(nameList.get(1)+nameList.get(2));
+        System.out.print("From where? ");
+        int fromWhere = Integer.valueOf(scanner.nextLine());
+        
+        System.out.print("To where? ");
+        int toWhere = Integer.valueOf(scanner.nextLine());
+
+        int listLength = nameList.size();
+
+        if (fromWhere <= 0 || toWhere >= listLength-1){
+            for (; fromWhere <= toWhere; fromWhere++){
+                System.out.println(nameList.get(fromWhere));
+            }
+        }else{
+            System.out.println("You are writing a nonexisting index");
+        }
     }
 }
