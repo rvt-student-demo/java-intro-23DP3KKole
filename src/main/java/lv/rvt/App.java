58bdd96a7bbef31305e0c806cc25b8c75fbb38cf
23DP3KKole <166013436+23DP3KKole@users.noreply.github.com>
2024-11-13 08:45:33 +0000

    test

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,20 +4,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        Account artosAccount = new Account("Arto's account", 100.00);
-        Account artosSwissAccount = new Account("Arto's account in Switzerland", 1000000.00);
-
-        System.out.println("Initial state");
-        System.out.println(artosAccount);
-        System.out.println(artosSwissAccount);
-
-        artosAccount.withdraw(20);
-        System.out.println("The balance of Arto's account is now: " + artosAccount.balance());
-        artosSwissAccount.deposit(200);
-        System.out.println("The balance of Arto's other account is now: " + artosSwissAccount.balance());
-
-        System.out.println("End state");
-        System.out.println(artosAccount);
-        System.out.println(artosSwissAccount);
+        Statistics statistics = new Statistics();
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
     }
 }
---
+++ src/main/java/lv/rvt/Statistics.java
@@ -0,0 +1,19 @@
+package lv.rvt;
+
+public class Statistics {
+    private int count;
+    private int numberCount;
+
+    public Statistics() {
+        // initialize the variable numberCount here
+        this.numberCount = numberCount;
+    }
+    public void addNumber(int number) {
+        // write code here
+    }
+
+    public int getCount() {
+        // write code here
+        return 1;
+    }
+}
