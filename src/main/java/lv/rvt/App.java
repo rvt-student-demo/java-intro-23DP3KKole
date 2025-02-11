1b8504b13750bdb78f070a7f55c87123db6c03ca
23DP3KKole <166013436+23DP3KKole@users.noreply.github.com>
2025-02-11 07:48:37 +0000

    Warehousing (7 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,23 +1,16 @@
 package lv.rvt;
 
-import java.io.BufferedReader;
-
 public class App 
 {
     public static void main( String[] args ) throws Exception
     {
         //BufferedReader reader = Helper.getReader("persons.csv");
+        
+        ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
+        juice.takeFromWarehouse(11.3);
+        juice.addToWarehouse(1.0);
+        //System.out.println(juice.history()); // [1000.0, 988.7, 989.7]
 
-        Account matthewAccount = new Account("Matthew", 1000.0);
-        Account myAccount = new Account("Me", 0.0);
-
-        matthewAccount.withdraw(100.0);
-        myAccount.deposit(100.0);
-
-        System.out.println(matthewAccount.toString());
-        System.out.println(myAccount.toString());
-
-        matthewAccount.printAccount();
-        myAccount.printAccount();
+        juice.printAnalysis();
     }
 }
